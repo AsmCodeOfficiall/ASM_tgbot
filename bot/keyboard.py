6057ce8587155f5f5a5242bde4a2962c6ea2985f
Mafusail2020 <krutkev00@gmail.com>
2026-05-21 14:50:01 +0300

    feat: add inline keyboard and welcoming message

--- bot/keyboard.py
+++ bot/keyboard.py
@@ -1,1 +1,10 @@
-# Gleb: кнопка Web App — URL з WEBAPP_URL у .env
+# Ivan: кнопка Web App — URL з WEBAPP_URL у .env
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+from config import settings
+
+
+keyboard_start = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text="Open website", url=settings.WEBAPP_URL)]
+    ])
--- bot/messages.py
+++ bot/messages.py
@@ -1,2 +1,6 @@
-MSG_REPORT_REQUEST = "Send your daily standup"
+MSG_REPORT_REQUEST = "Відправ репот за день"
+
 MSG_REPORT_ACCEPTED = "Дякую! Твій звіт прийнято. О 21:00 він буде відправлений тімліду."
+
+
+MSG_START = "Відкрити дашборд"
