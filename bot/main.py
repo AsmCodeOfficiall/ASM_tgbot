d7ac30c2f58583cfcef3a17b595ed3925d9b8c06
Bogdan <b.hrytsenko@ukma.edu.ua>
2026-05-21 19:57:03 +0300

    chore: remove all comments from Dockerfile and bot/main.py

--- bot/main.py
+++ bot/main.py
@@ -1,27 +1,20 @@
-# Ivan: старт бота, polling, підключити handlers і scheduler
 import asyncio
 import logging
-from api.db import init_db
 
 from bot.scheduler import scheduler
 from bot.bot_dp import dp, bot
-
 from bot.handlers import router
 from bot.scheduler import router_scheduler
-
 
 async def main():
     dp.include_router(router=router)
     dp.include_router(router=router_scheduler)
-
     scheduler.start()
-    # await init_db() # Fledif inits db in run.py lifespan, so bot doesn't need to
     await dp.start_polling(bot)
-
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
-        print("[#] PROG EXIT")
+        pass
