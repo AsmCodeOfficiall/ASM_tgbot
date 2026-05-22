210cbf8e4d41ea00e0b186a897d0bf3603687c0a
Bogdan <b.hrytsenko@ukma.edu.ua>
2026-05-23 02:44:40 +0300

    Fix bot config Pydantic validation error

--- bot/config.py
+++ bot/config.py
@@ -3,10 +3,10 @@ from pydantic_settings import BaseSettings
 
 class __Settings(BaseSettings):
     BOT_TOKEN: str
-    WEBAPP_URL: str
-    TELEGRAM_REPORT_CHAT_ID: str
-    TELEGRAM_ALERT_CHAT_ID: str
-    GITHUB_WEBHOOK_SECRET: str
+    WEBAPP_URL: str = ""
+    TELEGRAM_REPORT_CHAT_ID: str = ""
+    TELEGRAM_ALERT_CHAT_ID: str = ""
+    GITHUB_WEBHOOK_SECRET: str = ""
 
     class Config:
         env_file = ".env"
